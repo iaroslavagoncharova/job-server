630f47e294e3f507b2e26be0493e7ac7377f5feb
mannals <annamalassu@gmail.com>
2024-03-25 14:48:11 +0200

    added DB types and expanded chats model

--- hybrid-types/DBTypes.ts
+++ hybrid-types/DBTypes.ts
@@ -1,3 +1,8 @@
+export type UserLevel = {
+    level_id: number;
+    level_name: string;
+};
+
 export type User = {
     user_id: number;
     username: string;
@@ -20,4 +25,46 @@ export type TokenUser = {
     user_id: number;
     user_level_id: number;
     user_type: string;
+};
+
+export type TokenContent = Pick<User, 'user_id'> & Pick<UserLevel, 'level_name'>;
+
+export type Application = {
+    application_id: number;
+    user_id: number;
+    job_id: number;
+    status: string;
+    application_text: string | null;
+    created_at: Date | string;
+};
+
+export type ApplicationLink = {
+    link_id: number;
+    application_id: number;
+    link: string;
+};
+
+export type Chat = {
+    chat_id: number;
+    created_at: Date | string;
+};
+
+export type UserChat = {
+    user_id: number;
+    chat_id: number;
+};
+
+export type Message = {
+    message_id: number;
+    user_id: number;
+    chat_id: number;
+    message_text: string;
+    sent_at: Date | string;
+};
+
+export type Match = {
+    match_id: number;
+    user1_id: number;
+    user2_id: number;
+    created_at: Date | string;
 };
